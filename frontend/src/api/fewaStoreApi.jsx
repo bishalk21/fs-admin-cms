b452c2d10c40fc83a7b40bb174a27487cb1031e8
Your Name <karkibishal00@gmail.com>
2023-10-10 11:41:13 +1100

    admin cms ready to deploy check

--- frontend/src/api/fewaStoreApi.jsx
+++ frontend/src/api/fewaStoreApi.jsx
@@ -1,5 +1,10 @@
-import Axios from "axios";
+import axios from "axios";
 
-export default Axios.create({
-  baseURL: import.meta.env.VITE_APP_API_ENDPOINT,
+const fewaStoreApi = axios.create({
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.VITE_APP_API_ENDPOINT
+      : "http://localhost:3300",
 });
+
+export default fewaStoreApi;
