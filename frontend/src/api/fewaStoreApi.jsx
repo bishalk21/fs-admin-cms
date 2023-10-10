69bc1e6770dc28aed4e0cef143d59e52b922fd0b
Your Name <karkibishal00@gmail.com>
2023-10-10 11:52:34 +1100

    admin cms ready to deploy check

--- frontend/src/api/fewaStoreApi.jsx
+++ frontend/src/api/fewaStoreApi.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const fewaStoreApi = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? process.env.VITE_APP_API_ENDPOINT
+      ? "https://fewa-store-api.vercel.app"
       : "http://localhost:3300",
 });
 
